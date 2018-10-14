cd7ae2ccbeb87672cf022547a062e5d0ed3f10f0
Lutfy <37364743+lutfiyanti@users.noreply.github.com>
2018-10-14 02:56:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ def handle_message(event):
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
 
-line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text+' '+profile.display_name))
+
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text+' '+profile.display_name))
 
 import os
 if __name__ == "__main__":
