eb4557b87cd74ddb2007b2673c424d41a83150c6
Lutfy <37364743+lutfiyanti@users.noreply.github.com>
2018-10-18 22:43:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -43,75 +43,30 @@ handler = WebhookHandler('acd0a3afda7cfff11db1ef4a0c81e498')
 notes = {}
 
 #input mencari
-def carikamar(nomor):
-    URLkamar = "http://www.aditmasih.tk/api_lutfyh/show.php?nomor=" + nomor
-    r = requests.get(URLkamar)
-    data = r.json()
+def cariproduk(nama_produk):
+    URLproduk = "http://api.agusadiyanto.net/halal/?menu=nama_produk&query=" + nama_produk
+    r = requests.get(URLproduk)
+    produk = r.json()
     err = "data tidak ditemukan"
     
-    flag = data['flag']
-    if(flag == "1"):
-        nomor = data['kamar'][0]['nomor']
-        status = data['kamar'][0]['status']
-        tipe = data['kamar'][0]['tipe']
-        harga = data['kamar'][0]['harga']
-        atas_nama = data['kamar'][0]['atas_nama']
-        hp = data['kamar'][0]['hp']
+    status = produk['status']
+    if(status == "success"):
+        nama_produk = produk['data'][0]['nama_produk']
+        nomor_sertifikat = produk['data'][0]['nomor_sertifikat']
+        nama_produsen = produk['data'][0]['nama_produsen']
+        berlaku_hingga = produk['data'][0]['berlaku_hingga']
+        tes = produk['data'][0]['tes']
         
 
         # munculin semua, ga rapi, ada 'u' nya
         # all_data = data['teman'][0]
-        data= "Nomor kamar: "+nomor+"\nStatus : "+status+"\nTipe kamar: "+tipe+"\nHarga kamar : "+harga+"\nPemilik "+atas_nama+"\n Nomor Hp :" +hp
-        return data
+        data= "Nama Produk : "+nama_produk+"\nNomor Sertifikat : "+nomor_sertifikat+"\nNama Produsen : "+nama_produsen+"\nBerlaku Hingga : "+berlaku_hingga+"\nTes : "+tes
+        return print (data)
         # return all_data
 
-    elif(flag == "0"):
-        return err
+    elif(status == "error"):
+        return print (err)
 
-#INPUT DATA teman
-def inputkamar(nomor,status,tipe,harga,atas_nama,hp):
-    r = requests.post("http://www.aditmasih.tk/api_lutfyh/insert.php", data={'nomor': nomor, 'status': status, 'tipe': tipe, 'harga': harga, 'atas_nama':atas_nama, 'hp':hp})
-    data = r.json()
-
-    flag = data['flag']
-   
-    if(flag == "1"):
-        return 'Data '+' berhasil dimasukkan\n'
-    elif(flag == "0"):
-        return 'Data gagal dimasukkan\n'
-
-
-#DELETE DATA teman
-def hapuskamar(nomor):
-    r = requests.post("http://www.aditmasih.tk/api_lutfyh/delete.php", data={'nomor': nomor})
-    data = r.json()
-
-    flag = data['flag']
-   
-    if(flag == "1"):
-        return 'Data kamar nomor '+nomor+'atas nama'+atas_nama+' dengan kontak '+hp+' berhasil dihapus\n'
-    elif(flag == "0"):
-        return 'Data gagal dihapus\n'
-
-def updatekamar(hp_lama,nomor,status,tipe,harga,atas_nama,hp):
-    URLteman = "http://www.aditmasih.tk/api_lutfyh/show.php?hp=" + hp_lama
-    r = requests.get(URLkamar)
-    data = r.json()
-    err = "data tidak ditemukan"
-    hpLama=hp_lama
-    flag = data['flag']
-    if(flag == "1"):
-        r = requests.post("http://www.aditmasih.tk/api_lutfyh/update.php", data={'nomor': nomor, 'status': status, 'tipe': tipe, 'harga': harga, 'atas_nama':atas_nama, 'hp':hp, 'hpLama' :hpLama})
-        data = r.json()
-        flag = data['flag']
-
-        if(flag == "1"):
-            return 'Data kamar nomor '+nomor+ 'atas nama '+atas_nama+' berhasil diupdate\n'
-        elif(flag == "0"):
-            return 'Data gagal diupdate\n'
-
-    elif(flag == "0"):
-        return err
 
 # Post Request
 @app.route("/callback", methods=['POST'])
@@ -133,21 +88,12 @@ def handle_message(event):
     profile = line_bot_api.get_profile(sender)
 
     data=text.split('-')
-    if(data[0]=='lihat'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=carikamar(data[1])))
-    elif(data[0]=='tambah'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=inputkamar(data[1],data[2],data[3],data[4],data[5],data[6])))
-    elif(data[0]=='hapus'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hapuskamar(data[1])))
-    elif(data[0]=='ganti'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=updatekamar(data[1],data[2],data[3],data[4],data[5],data[6],data[7])))
-    elif(data[0]=='menu'):
-        menu = "Bot ini digunakan untuk data apartemen di suatu apartemen\nBagaimana cara kerjanya?\n1. lihat [nomor apartemen]\n2. tambah [nomor kamar]-[status kamar(isi/kosong)]-[tipe(studio/2BR/3BR dst)]-[harga per bulan]-[pemiliknya]-[Nomor Hp Pemilik]"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = menu))
+    if(produk[0]=='lihat'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=cariproduk(data[1])))
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Coba pakai keyword yang bener deh, ketik menu coba buat cek keywordnya"))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Coba pakai keyword yang bener deh, find-(nama produk)"))
 
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=port)
